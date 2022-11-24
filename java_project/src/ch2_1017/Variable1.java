6587a8ed3f146832b77782aabf4cb9499d29f493
0qkrwldud1 <0qkrwldud1@naver.com>
2022-11-24 14:42:56 +0900

    저장 test

--- java_project/src/ch2_1017/Variable1.java
+++ java_project/src/ch2_1017/Variable1.java
@@ -5,8 +5,16 @@ public class Variable1 {
 	public static void main(String[] args) {
 
 		int level;              //정수형 변수 level을 선언
-		level = 10;		//level 변수에 10을 대입
-		byte b1 = 12;
+		level = 10;		//level 변수에 10을 대입 초기화 최초의 변수에 값을 할당
+		byte b1 = 12;	// 선언과 동시에 값을 할당 
+		//기본 데이터 타입 primitive type.
+		//기본 타입을 제외한 타입을 참조형타입 참조형을 많이 쓴다.
+		//참조형 타입 : 클래스타입
+		//class person
+		//참조형 타입으로 선언하게되면.
+		//person p1; 선언 만.
+		//p1 = new person(); 기본 생성자 호출
+		//생성자의 역할은 값을 초기화하는 역할.
 		System.out.println(level); 
 	}
 

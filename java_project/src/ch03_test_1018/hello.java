a336497ec0cabd8083bbcaf4602eafb117e5f0a3
0qkrwldud1 <0qkrwldud1@naver.com>
2022-11-24 12:38:47 +0900

    저장1

---
+++ java_project/src/ch03_test_1018/hello.java
@@ -0,0 +1,16 @@
+package ch03_test_1018;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 실행 ctrl + f11
+		// 보통은 ctrl + /로 주석처리.
+		// 여러줄 주석은 ctrl + shift + /
+		// 자동으로 정렬 ctrl + shift + f
+		System.out.print("hello 이상용 자바");
+		System.out.print("hello 이상용 자바");
+		System.out.print("hello 이상용 자바");
+	}
+
+}

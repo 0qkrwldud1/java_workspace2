a32781a903db62f3166f399e8af3f224d106404b
0qkrwldud1 <0qkrwldud1@naver.com>
2022-11-24 14:14:07 +0900

    test 저장

--- java_project/src/ch03_test_1018/hello2.java
+++ java_project/src/ch03_test_1018/hello2.java
@@ -5,7 +5,7 @@ public class hello2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// 실행 ctrl + f11
-		// 보통은 ctrl + /로 주석처리..
+		// 보통은 ctrl + /로 주석처리
 		// 여러줄 주석은 ctrl + shift + /
 		// 자동으로 정렬 ctrl + shift + f
 		System.out.print("hello 이상용 자바");

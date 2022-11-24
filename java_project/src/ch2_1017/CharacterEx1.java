af05f33f3f22de72dad7e64cae362925755b2e76
0qkrwldud1 <0qkrwldud1@naver.com>
2022-11-24 15:51:14 +0900

    저장 2

--- java_project/src/ch2_1017/CharacterEx1.java
+++ java_project/src/ch2_1017/CharacterEx1.java
@@ -7,7 +7,7 @@ public class CharacterEx1 {
 		System.out.println(ch1);
 		//ch1 변수를 형변환, 명시적 형변환.  55
 		// 캐스팅 연산자를 갑자기 사용하고 있음. 당황하지 마세요.
-		//7장 까지는 가볍게 들어보시고, 복습하면 됩니다. 
+		//7장 까지는 가볍게 들어보시고, 복습하면 됩니다.ㅇㅇㅇ 
 		System.out.println((int)ch1);
 		System.out.println("=====================");
 		char ch2 = 66;

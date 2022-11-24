fe7b71161f822138372ab4b1997f7edbf303548d
0qkrwldud1 <0qkrwldud1@naver.com>
2022-11-24 21:10:14 +0900

    저장 확인

--- java_project/src/ch03_test_1018/hello2.java
+++ java_project/src/ch03_test_1018/hello2.java
@@ -8,6 +8,7 @@ public class hello2 {
 		// 보통은 ctrl + /로 주석처리
 		// 여러줄 주석은 ctrl + shift + /
 		// 자동으로 정렬 ctrl + shift + f
+		// 연동확인
 		System.out.print("hello 이상용 자바");
 		System.out.print("hello 이상용 자바");
 		System.out.print("hello 이상용 자바");

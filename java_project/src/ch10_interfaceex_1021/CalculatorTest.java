28edd16c2d59047ca8a1fb6ef6ee88023d6ff2c8
0qkrwldud1 <0qkrwldud1@naver.com>
2022-12-08 16:39:26 +0900

    221208

--- java_project/src/ch10_interfaceex_1021/CalculatorTest.java
+++ java_project/src/ch10_interfaceex_1021/CalculatorTest.java
@@ -17,6 +17,7 @@ public class CalculatorTest {
 		int num2 = 5;
 		
 		CompleteCalc calc = new CompleteCalc();
+		
 		System.out.println(calc.add(num1, num2));
 		System.out.println(calc.substract(num1, num2));
 		System.out.println(calc.times(num1, num2));
